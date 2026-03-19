01311abdfdd371ac1af3238ce2863c5fe493c6d2
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2026-03-20 05:01:20 +0900

    feat: 공통 API 응답 래퍼 추가

---
+++ src/main/java/com/springcloud/client/malgncmsbe/common/response/ApiResponse.java
@@ -0,0 +1,34 @@
+package com.springcloud.client.malgncmsbe.common.response;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+    private boolean success;
+    private T data;
+    private String message;
+
+    public static <T> ApiResponse<T> success(T data) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.success = true;
+        response.data = data;
+        return response;
+    }
+
+    public static <T> ApiResponse<T> success() {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.success = true;
+        return response;
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.success = false;
+        response.message = message;
+        return response;
+    }
+}
