554d31e95bcf4d838b644f2c0dcbc1dde71065e3
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2026-03-20 05:02:05 +0900

    test: 스프링 컨텍스트 로드 테스트 추가

---
+++ src/test/java/com/springcloud/client/malgncmsbe/MalgnCmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.client.malgncmsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalgnCmsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
