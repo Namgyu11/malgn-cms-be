8634d16cf33c8718cb3e47d92b98b4d39836ba82
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2026-03-20 05:01:54 +0900

    feat: 콘텐츠 레포지토리 구현체 추가

---
+++ src/main/java/com/springcloud/client/malgncmsbe/contents/infrastructure/ContentsJpaRepository.java
@@ -0,0 +1,14 @@
+package com.springcloud.client.malgncmsbe.contents.infrastructure;
+
+import com.springcloud.client.malgncmsbe.contents.domain.Contents;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+interface ContentsJpaRepository extends JpaRepository<Contents, Long> {
+
+    @Modifying
+    @Query("UPDATE Contents c SET c.viewCount = c.viewCount + 1 WHERE c.id = :id")
+    void incrementViewCount(@Param("id") Long id);
+}
---
+++ src/main/java/com/springcloud/client/malgncmsbe/contents/infrastructure/ContentsRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.springcloud.client.malgncmsbe.contents.infrastructure;
+
+import com.springcloud.client.malgncmsbe.contents.domain.Contents;
+import com.springcloud.client.malgncmsbe.contents.domain.ContentsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ContentsRepositoryImpl implements ContentsRepository {
+
+    private final ContentsJpaRepository contentsJpaRepository;
+
+    @Override
+    public Contents save(Contents contents) {
+        return contentsJpaRepository.save(contents);
+    }
+
+    @Override
+    public Optional<Contents> findById(Long id) {
+        return contentsJpaRepository.findById(id);
+    }
+
+    @Override
+    public Page<Contents> findAll(Pageable pageable) {
+        return contentsJpaRepository.findAll(pageable);
+    }
+
+    @Override
+    public void delete(Contents contents) {
+        contentsJpaRepository.delete(contents);
+    }
+
+    @Override
+    public void incrementViewCount(Long id) {
+        contentsJpaRepository.incrementViewCount(id);
+    }
+}
