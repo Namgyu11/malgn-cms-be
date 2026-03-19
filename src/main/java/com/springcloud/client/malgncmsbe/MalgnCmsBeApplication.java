abc1631d9823d115a54c5a5ba270a3f7d319bb13
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2026-03-20 05:01:08 +0900

    chore: 그래들 래퍼 및 스프링 부트 진입점 추가

---
+++ src/main/java/com/springcloud/client/malgncmsbe/MalgnCmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.client.malgncmsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalgnCmsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalgnCmsBeApplication.class, args);
+    }
+
+}
